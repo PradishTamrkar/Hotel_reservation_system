ff5c51928230ff7a80a8571d7e0e3bc9c10c4ca5
Pradish Tamrakar <tmkr.pradish@gmail.com>
2025-11-25 22:56:43 +0545

    updain=ting the prod db

--- server/config/db.js
+++ server/config/db.js
@@ -1,13 +1,19 @@
 const { Sequelize } = require('sequelize');
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = require('./env');
 console.log('DB_USER:', DB_USER);
-console.log('DB_PASSWORD:', DB_PASSWORD);
+console.log('DB_PASSWORD:', DB_PASSWORD ? '***' : 'undefined');
 
 const sequelize = new Sequelize(DB_NAME, DB_USER, String(DB_PASSWORD), {
   host: DB_HOST,
   port: DB_PORT,
   dialect: 'postgres',
-  logging: false
+  logging: false,
+  dialectOptions:{
+    ssl:{
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
